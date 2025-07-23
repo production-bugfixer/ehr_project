46d5d284bbdb18949dac82f36a70832c00d60ec2
animsamanta <animsamanta@deloitte.com>
2025-07-23 18:09:16 +0530

    Initial commit: All microservices and scripts

--- registerservice/src/main/java/com/health/registerservice/RegisterserviceApplication.java
+++ registerservice/src/main/java/com/health/registerservice/RegisterserviceApplication.java
@@ -1,11 +1,14 @@
 package com.health.registerservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAutoConfiguration(exclude = { SystemMetricsAutoConfiguration.class })
 public class RegisterserviceApplication {
 
 	public static void main(String[] args) {
