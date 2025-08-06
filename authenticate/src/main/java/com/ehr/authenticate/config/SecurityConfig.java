981bd17c2076685ef366622610a30c8922542751
animsamanta <animsamanta@deloitte.com>
2025-08-06 18:08:58 +0530

    changed the tokenization

--- authenticate/src/main/java/com/ehr/authenticate/config/SecurityConfig.java
+++ authenticate/src/main/java/com/ehr/authenticate/config/SecurityConfig.java
@@ -1,29 +1,28 @@
 package com.ehr.authenticate.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .cors() 
-            .and()
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Optional: stateless
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/forgort-password/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/authorize/**")).permitAll()
-                .anyRequest().authenticated()
-            );
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    http
+	        .csrf(csrf -> csrf.disable())
+	        .authorizeHttpRequests(auth -> auth
+	            .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
+	            .requestMatchers(new AntPathRequestMatcher("/forgort-password/**")).permitAll()
+	            .requestMatchers(new AntPathRequestMatcher("/authorize/**")).permitAll()
+	            .anyRequest().authenticated()
+	        );
 
-        return http.build();
-    }
+	    return http.build();
+	}
 }
